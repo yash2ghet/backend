3f1fb300995f0896e222e89656b3abd6eed7b412
Yashvi Ghetiya <patelyasvi495@gmail.com>
2026-06-08 00:44:19 +0530

    fix vercel express deployment

--- index.ts
+++ index.ts
@@ -4,40 +4,21 @@ dotenv.config();
 import express from "express";
 import v1Routers from "./src/routes/v1";
 import cors from "cors";
-import mongoose from 'mongoose';
-
-console.log(process.env.EMAIL_USER);
-console.log(process.env.EMAIL_PASS);
-console.log(process.env.JWT_SECRET);
-console.log(process.env.MONGODB_URI);
+import mongoose from "mongoose";
 
 const app = express();
 
-async function connectDB(): Promise<void> {
-
-    await mongoose.connect(process.env.MONGODB_URI as string);
-
-    console.log("MongoDB connected successfully");
-
-    mongoose.connection.on("error", (err: Error) => {
-        console.error(`MongoDB connection error: ${err.message}`);
-    })
+async function connectDB() {
+  await mongoose.connect(process.env.MONGODB_URI as string);
+  console.log("MongoDB connected successfully");
 }
 
-async function main() {
-    await connectDB();
+connectDB().catch(console.error);
 
-    app.use(cors({ origin: "*" }));
-    app.use(express.json({ limit: "10kb"}));
-    app.use(express.urlencoded({ extended: true }));
-    app.use("/api/v1", v1Routers);
+app.use(cors({ origin: "*" }));
+app.use(express.json({ limit: "10kb" }));
+app.use(express.urlencoded({ extended: true }));
 
-    app.listen(4000, () => {
-        console.log("Server is running on http://localhost:4000");
-    });
-}
+app.use("/api/v1", v1Routers);
 
-main().catch((error) => {
-    console.error(error);
-    process.exit(1);
-});
+export default app;
